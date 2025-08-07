bc5e74471cb2c1620de34281a5a5c9e785b8f76e
Wong Shouhao <wongshouhao@outlook.com>
2025-08-08 07:07:26 +0800

    Remove debt share (#6)
    * Add decrease debt share
    
    * Add decrease debt test
    
    * Add open delta decrease
    
    * Add zero deposit

--- src/libraries/LicredityStateView.sol
+++ src/libraries/LicredityStateView.sol
@@ -6,9 +6,25 @@ import {ILicredity} from "@licredity-v1-core/interfaces/ILicredity.sol";
 library LicredityStateView {
     uint256 public constant TOTAL_DEBT_SHARE_OFFSET = 16;
     uint256 public constant TOTAL_DEBT_BALANCE_OFFSET = 17;
+    uint256 public constant POSITIONS_OFFSET = 25;
 
     function getTotalDebt(ILicredity manager) internal view returns (uint256 totalShares, uint256 totalAssets) {
         totalShares = uint256(manager.extsload(bytes32(TOTAL_DEBT_SHARE_OFFSET)));
         totalAssets = uint256(manager.extsload(bytes32(TOTAL_DEBT_BALANCE_OFFSET)));
+    }
+
+    function _getPositionSlot(uint256 positionId) internal pure returns (bytes32 slot) {
+        assembly ("memory-safe") {
+            mstore(0x00, positionId)
+            mstore(0x20, POSITIONS_OFFSET)
+            slot := keccak256(0x00, 0x40)
+        }
+    }
+
+    function getPositionDebtShare(ILicredity manager, uint256 positionId) internal view returns (uint256 debtShare) {
+        bytes32 stateSlot = _getPositionSlot(positionId);
+        bytes32 debtSlot = bytes32(uint256(stateSlot) + 1);
+
+        debtShare = uint256(manager.extsload(debtSlot));
     }
 }
