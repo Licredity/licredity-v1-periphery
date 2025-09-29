fe59cac660b25d1512273da080063e2d5491e978
wangshouh <wongshouhao@outlook.com>
2025-09-29 21:08:08 +0700

    Delete unused import

--- src/interfaces/ILicredityAccount.sol
+++ src/interfaces/ILicredityAccount.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.24;
 
-import {ActionsData} from "src/types/Actions.sol";
 import {NonFungible} from "@licredity-v1-core/types/NonFungible.sol";
 import {Currency} from "@uniswap-v4-core/types/Currency.sol";
 import {IPositionManagerConfig} from "./IPositionManagerConfig.sol";
